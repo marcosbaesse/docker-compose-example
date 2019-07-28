228761be820861cd35102d593911e18f7a0ed7c9
Marcos Baesse <marcosbaesse@gmail.com>
2019-07-28 20:52:47 -0300

    Add rabbitmq service over docker

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RabbitMQServer } from './rabbitmq-server';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
