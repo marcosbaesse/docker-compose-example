80c6e3a3eaa59b222b55a003eb9ca0ceca931b80
Marcos Baesse <marcosbaesse@gmail.com>
2019-07-28 12:59:10 -0300

    configure server main script

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RabbitMQServer } from './rabbitmq-server';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    strategy: new RabbitMQServer('amqp://localhost', 'channel'),
+  });
 }
 bootstrap();
